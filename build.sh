51f00c55783ea82b09cebeb5943930c63595d2c2
chairmakan <aruviinstituteoflearning@gmail.com>
2025-10-08 21:24:51 +0530

    second commit

---
+++ build.sh
@@ -0,0 +1,14 @@
+# ! /usr/bin/env bash
+# exit on error
+
+set -o errexit
+
+# modify the line as needed for your package manager(pip, poetry, etc.)
+pip install -r requirements.txt
+
+# convert static assets files
+
+python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+python manage.py migrate
